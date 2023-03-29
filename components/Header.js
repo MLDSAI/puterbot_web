882f36bffee249c9c0ef9fd399428d5b9bfe04ce
Richard Abrich <richard.abrich@gmail.com>
2023-03-29 12:00:21 -0400

    fix header styles:

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,9 @@
+import styles from './Header.module.css'
+
 export default function Header({ title }) {
   return (
     <>
-      <div style="text-align:center">
+      <div className={styles.header}>
           <img src="/favicon.svg" alt="Puterbot Logo" />
           <h1 className="title">{title}</h1>
           <h2>AI-First Process Automation</h2>
